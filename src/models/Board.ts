2b68b2e345273938fc2f69df3639b6590fb9c6ba
Kirill Karchenkov <karjobmail@rambler.ru>
2025-08-08 15:17:33 +0300

    add figures

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,6 +1,11 @@
 import { Cell } from "./Cell"
 import { Colors } from "./Colors"
+import { Bishop } from "./figures/Bishop"
+import { King } from "./figures/King"
+import { Knight } from "./figures/Knight"
+import { Pawn } from "./figures/Pawn"
 import { Queen } from "./figures/Queen"
+import { Rook } from "./figures/Rook"
 
 export class Board {
     cells: Cell[][] = []
@@ -20,12 +25,59 @@ export class Board {
 
     }
 
+
+    public addFigures() {
+     
+        this.addPawn()
+        this.addQueen()
+        this.addKing()
+        this.addBishop()
+        this.addRook()
+        this.addKnight()
+        
+    }
+
+    addPawn() {
+        for (let i = 0; i < 8; i++) {
+            new Pawn(Colors.BLACK, this.getCell(i, 1))
+            new Pawn(Colors.WHITE, this.getCell(i, 6))
+        }
+    }
+
+    addQueen() {
+        new Queen(Colors.WHITE, this.getCell(3, 7));
+        new Queen(Colors.BLACK, this.getCell(3, 0))
+    }
+
+    addKing() {
+        new King(Colors.WHITE, this.getCell(4, 7));
+        new King(Colors.BLACK, this.getCell(4, 0))
+    }
+
+    addBishop() {
+        new Bishop(Colors.WHITE, this.getCell(2, 7));
+        new Bishop(Colors.WHITE, this.getCell(5, 7));
+        new Bishop(Colors.BLACK, this.getCell(2, 0));
+        new Bishop(Colors.BLACK, this.getCell(5, 0));
+    }
+
+    addRook() {
+        new Rook(Colors.WHITE, this.getCell(0, 7));
+        new Rook(Colors.WHITE, this.getCell(7, 7));
+        new Rook(Colors.BLACK, this.getCell(0, 0));
+        new Rook(Colors.BLACK, this.getCell(7, 0));
+    }
+
+    addKnight() {
+        new Knight(Colors.WHITE, this.getCell(1, 7));
+        new Knight(Colors.WHITE, this.getCell(6, 7));
+        new Knight(Colors.BLACK, this.getCell(1, 0));
+        new Knight(Colors.BLACK, this.getCell(6, 0));
+    }
+
     public getCell(x: number, y: number) {
         return this.cells[y][x]
     }
 
-    public addFigures() {
-        new Queen(Colors.WHITE, this.getCell(3, 0));
-        new Queen(Colors.BLACK, this.getCell(3, 7))
-    }
+    
 }
